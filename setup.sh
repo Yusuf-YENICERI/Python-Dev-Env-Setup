c846b6d4ab6e65489c54abe0741c0a3fb7b74204
Yusuf-YENICERI <joshard20917@gmail.com>
2023-01-24 21:46:16 +0000

    completed, inshaAllah

---
+++ setup.sh
@@ -0,0 +1,31 @@
+
+echo "updating and upgrading packages"
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "installing python"
+sudo apt-get install python3
+sudo apt-get install python3-pip
+
+echo "installing virtualenv"
+pip3 install virtualenv
+
+echo "installing wget"
+sudo apt-get install wget
+
+echo "installing vscode"
+
+echo "please type which one do you need? x64, arm32 or arm64 (default=x64)"
+read architecture
+
+url=""
+if [[ $architecture == "x64" ]]; then
+url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+elif [[ $architecture == "arm32" ]]; then
+url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-armhf"
+else
+url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64"
+fi
+
+wget -O code.deb $url
+sudo apt-get install ./code.deb
